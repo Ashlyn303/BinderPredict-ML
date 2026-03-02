4d690c2e0167635387d44e4e361c38dc9b6267a3
Ashlyn303 <szyhsuan@gmail.com>
2026-03-02 14:38:12 -0700

    Add detailed logging and robust aliases to fix Manual model loading

--- data_loader.py
+++ data_loader.py
@@ -1,1 +1,8 @@
+import sys
+from src import data_loader
+
+# Make sure 'data_loader' is registered in sys.modules
+sys.modules['data_loader'] = data_loader
+
+# Expose everything to the root namespace
 from src.data_loader import *
